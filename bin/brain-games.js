d4b6dd7134e60bb494762db5cf0ee9459270fd54
ALEKSEI <alex@ALEKSEIs-MacBook-Pro.local>
2025-05-21 18:54:29 +0300

    Fix brain-games adding welcoming line at the start

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { nameStore } from "../src/index.js";
+import { start } from "../games/brainGames.js";
 
-nameStore.askName();
+start();
---
+++ games/brainGames.js
@@ -0,0 +1,9 @@
+import { nameStore } from "../src/index.js";
+
+const start = () => {
+
+    console.log(`Welcome to the Brain Games!`)
+    nameStore.askName();
+}
+
+export { start };
